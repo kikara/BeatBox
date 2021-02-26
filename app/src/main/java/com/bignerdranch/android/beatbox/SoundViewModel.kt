84ff247a51d93b78e8c3297475d200e03949f6d2
Artur Torbogoshev <torbogoshev@yandex.ru>
2021-02-26 06:58:50 +0200

    first commit

---
+++ app/src/main/java/com/bignerdranch/android/beatbox/SoundViewModel.kt
@@ -0,0 +1,20 @@
+package com.bignerdranch.android.beatbox
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class SoundViewModel: BaseObservable() {
+
+
+
+    var sound: Sound? = null
+        set(sound) {
+            field = sound
+//            Оповещает класс привязки о том что все Bindable свойства обновлены
+            notifyChange()
+        }
+    @get: Bindable
+    val title: String?
+        get() = sound?.name
+
+}
